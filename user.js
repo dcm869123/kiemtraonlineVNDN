0bbc62d68146bfa38a1f9e44016e02e4e2e72fe0
dcm869123 <danvipmd214@gmail.com>
2026-06-27 18:07:05 +0700

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 const USERS_DATABASE = [
     { username: "L4M", password: "mocmeo090", role: "admin", fullname: "L4M" },
     { username: "boidongnai", password: "123456", role: "admin", fullname: "Boideptrai" },
-    { username: "kru_alien", password: "123456", role: "admin", fullname: "admin ngheo ngheo" },
+    { username: "kru_alien", password: "11223344", role: "admin", fullname: "admin ngheo ngheo" },
     { username: "student2", password: "123", role: "student", fullname: "Trần Thị B" }
 ];
 
